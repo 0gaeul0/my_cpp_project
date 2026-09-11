d157acde5c2941eb29e02ebc4b4aaf05d6ff4b30
Sofia Kolomeets <sofia_1710@mail.ru>
2026-09-11 20:59:48 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-int main () {
-    std::cout << "Hello, my name is Sonya. I am from IU5-13/4!" << std::endl;
-    return 0;
+
+int main() {
+std::cout << "Updated message!" << std::endl;
+return 0;
 }
